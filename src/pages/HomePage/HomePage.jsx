fc8470fa9682ea708e460e5b6a700d725d67cc54
Anastasiiya Onyshchuk <shapoval.anastasiiya@gmail.com>
2024-10-17 18:29:46 +0300

    edit hero styles

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,22 @@
+import { NavLink } from "react-router-dom";
+import css from "./HomePage.module.css";
+
+const HomePage = () => {
+  return (
+    <>
+      <div className={css.thumb}>
+        <div className={css.container}>
+          <h1 className={css.title}>Campers of your dreams</h1>
+          <p className={css.text}>
+            You can find everything you want in our catalog
+          </p>
+        </div>
+        <NavLink to="/catalog" className={css.link}>
+          View Now
+        </NavLink>
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
